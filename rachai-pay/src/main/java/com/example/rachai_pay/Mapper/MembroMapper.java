7194100b91a7ae47194d954f8efb4bfad88ac5ff
cauacouto <cauacouto022@gmail.com>
2026-05-27 22:41:53 -0300

    update: adicionar mapping

--- rachai-pay/src/main/java/com/example/rachai_pay/Mapper/MembroMapper.java
+++ rachai-pay/src/main/java/com/example/rachai_pay/Mapper/MembroMapper.java
@@ -3,13 +3,15 @@ package com.example.rachai_pay.Mapper;
 import com.example.rachai_pay.Dtos.MembroGrupDto;
 import com.example.rachai_pay.domin.MembrosGrup;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MembroMapper {
 
 
 
-
+    @Mapping(target = "usuarioId", source = "usuario.id")
+    @Mapping(target = "grupoId", source = "grupo.id")
     MembroGrupDto toDto(MembrosGrup entity);
 
     MembrosGrup toEntity(MembroGrupDto response);
